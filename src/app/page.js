c91b1732a98c8da9a571caa3c921be62e2383ce7
chirag <chiragsinghal04@gmail.com>
2025-02-21 23:49:34 +0530

    2

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,7 @@ import { Dialog, DialogPanel } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import Image from 'next/image'
 import Link from 'next/link'
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
 
 const navigation = [
   { name: 'Product', href: '#' },
@@ -20,8 +21,9 @@ export default function Home() {
     <div>
       <header className="absolute inset-x-0 top-0 z-50">
         <nav aria-label="Global" className="flex items-center justify-between p-6 lg:px-8">
+          {/* desktop view */}
           <div className="flex lg:flex-1">
-            <Link href="#" className="-m-2 p-3 border-black border">
+            <Link href="#" className="-m-2 p-3 ">
               <span className="sr-only">MedRem</span>
               <Image
                 alt="logo"
@@ -32,6 +34,7 @@ export default function Home() {
               />
             </Link>
           </div>
+          {/* mobile view */}
           <div className="flex lg:hidden">
             <button
               type="button"
@@ -42,29 +45,39 @@ export default function Home() {
               <Bars3Icon aria-hidden="true" className="size-6" />
             </button>
           </div>
+          {/* desktop */}
           <div className="hidden lg:flex lg:gap-x-12">
             {navigation.map((item) => (
-              <Link key={item.name} href={item.href} className="text-sm/6 font-semibold text-gray-900">
+              <Link key={item.name} href={item.href} className="text-lg font-semibold text-gray-900">
                 {item.name}
               </Link>
             ))}
+            
           </div>
-          <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-            <Link href="#" className="text-sm/6 font-semibold text-gray-900">
-              Log in <span aria-hidden="true">&rarr;</span>
-            </Link>
+          <div className="hidden lg:flex lg:flex-1 lg:justify-end mr-10">
+            <div className="text-md font-semibold text-gray-900">
+            <SignedIn>
+                      <UserButton />
+                    </SignedIn>
+                    <SignedOut>
+                    <SignInButton/>
+                    </SignedOut>
+              
+            </div>
           </div>
         </nav>
         <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
           <div className="fixed inset-0 z-50" />
-          <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-blue-100 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
             <div className="flex items-center justify-between">
               <Link href="#" className="-m-1.5 p-1.5">
                 <span className="sr-only">Your Company</span>
                 <Image
                   alt=""
                   src="/assets/images/logo.png"
-                  className="h-8 w-auto"
+                  height={100}
+                  width={100}
+                  className='rounded-xl'
                 />
               </Link>
               <button
@@ -90,12 +103,16 @@ export default function Home() {
                   ))}
                 </div>
                 <div className="py-6">
-                  <Link
-                    href="#"
+                  <div
                     className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
                   >
-                    Log in
-                  </Link>
+                    <SignedIn>
+                      <UserButton/>
+                    </SignedIn>
+                    <SignedOut>
+                    <SignInButton/>
+                    </SignedOut>
+                  </div>
                 </div>
               </div>
             </div>
@@ -113,33 +130,29 @@ export default function Home() {
               clipPath:
                 'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
             }}
-            className="relative left-[calc(50%-11rem)] aspect-1155/678 w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-90 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+           className="relative left-[calc(50%-11rem)] w-[36.125rem] h-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#b2dbff] to-[#4a89e6] opacity-90"
           />
         </div>
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
           <div className="hidden sm:mb-8 sm:flex sm:justify-center">
             <div className="relative rounded-full px-3 py-1 text-sm/6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-              Announcing our next round of funding.{' '}
-              <a href="#" className="font-semibold text-indigo-600">
-                <span aria-hidden="true" className="absolute inset-0" />
-                Read more <span aria-hidden="true">&rarr;</span>
-              </a>
+            Empowering Health, Simplifying Care. 💙
             </div>
           </div>
           <div className="text-center">
             <h1 className="text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">
-              Data to enrich your online business
+              Stay Healthy, Stay on Track
             </h1>
             <p className="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
-              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
-              fugiat veniam occaecat.
+              Never miss a dose with our smart pillbox and real time notifications.
+              Take control of you medications with AI powered reminders and prescription tracking
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <Link
                 href="#"
                 className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                Get started
+                Add a Prescription
               </Link>
               <Link href="#" className="text-sm/6 font-semibold text-gray-900">
                 Learn more <span aria-hidden="true">→</span>
---
+++ src/lib/firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDQV5dab2vwU1rdlxsfaSMTDtfgP0kOP2Q",
+  authDomain: "medrem-fbcb0.firebaseapp.com",
+  projectId: "medrem-fbcb0",
+  storageBucket: "medrem-fbcb0.firebasestorage.app",
+  messagingSenderId: "954226199500",
+  appId: "1:954226199500:web:6c134554da50db66dd6b69",
+  measurementId: "G-XW9DFRX8H7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db= getFirestore(app);
+const analytics = getAnalytics(app);
