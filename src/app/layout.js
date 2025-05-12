eb88c52838896b70500f23e3fc9116b18c30d665
chirag <chiragsinghal04@gmail.com>
2025-05-13 01:04:03 +0530

    push

--- src/app/layout.js
+++ src/app/layout.js
@@ -26,6 +26,20 @@ export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
     <html lang="en">
+      <head>
+        <Script
+          id="pushalert"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(d, t) {
+              var g = d.createElement(t),
+              s = d.getElementsByTagName(t)[0];
+              g.src = "https://cdn.pushalert.co/integrate_26bda26de01abbbea4803110fc2405cc.js";
+              s.parentNode.insertBefore(g, s);
+            })(document, "script");`,
+          }}
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
